5d5bdfdbea88bcd090e5e2676ed680fa6de9bd38
Tunnelblick <38663241+AppelsPen@users.noreply.github.com>
2020-12-20 17:02:32 +0100

    Initial commit
    Initial commit

---
+++ parent/endlessmirage-main/src/main/java/com/endlessmirage/main/config/ServiceConfig.java
@@ -0,0 +1,10 @@
+package com.endlessmirage.main.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.endlessmirage.*")
+public class ServiceConfig {
+
+}
